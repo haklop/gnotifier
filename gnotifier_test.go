cfdc3a3905a7f2ce72e458166c03d2ea25a71757
Eric Bellemon <eric.bellemon@gmail.com>
2014-07-26 00:42:34 +0200

    Typo

--- gnotifier_test.go
+++ gnotifier_test.go
@@ -7,7 +7,6 @@ import (
 func Test_Notification(t *testing.T) {
 	n := Notification("Hey", "Hello")
 
-	//assert defaults
 	if n.GetConfig().Title != "Hey" {
 		t.Error("NewNotification doesn't have a Title specified")
 	}
@@ -39,7 +38,6 @@ func Test_Push(t *testing.T) {
 	n := Notification("Title!", "Testing Push")
 	err := n.Push()
 
-	//assert defaults
 	if err != nil {
 		t.Error("Test_Push failed with error: ", err)
 	}
